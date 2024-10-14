2bcd6a9158ee4598317cec01b289a00235803589
MSI <hirun08072002@gmail.com>
2024-10-14 09:15:54 +0530

    finish musicplayergui and begin creating songs

--- src/musicplayergui.java
+++ src/musicplayergui.java
@@ -1,18 +1,26 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+import javax.swing.text.StyledEditorKit.BoldAction;
 
 public class musicplayergui extends JFrame {
     public static final Color FRAME_COLOR = Color.black;
     public static final Color TEXT_COLOR = Color.white;
+    
 
     public musicplayergui() {
         // Call the JFrame constructor and set the window title
@@ -41,20 +49,71 @@ public class musicplayergui extends JFrame {
     private void addGuiComponents() {
         addToolbar();
 
+        JLabel songImage = new JLabel(loadImage("src\\assets\\record.png"));
+        songImage.setBounds(0, 50, getWidth() - 20, 225);
+        add(songImage);
 
+        JLabel songTitle = new JLabel("Song Title");
+        songTitle.setBounds(0, 285, getWidth() - 10, 30);
+        songTitle.setFont(new Font("Dailog", Font.BOLD, 24));
+        songTitle.setForeground(TEXT_COLOR);
+        songTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songTitle);
+
+        JLabel songArtist = new JLabel("Artist");
+        songArtist.setBounds(0, 315, getWidth() - 10, 30);
+        songArtist.setFont(new Font("Dailog", Font.BOLD, 24));
+        songArtist.setForeground(TEXT_COLOR);
+        songArtist.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songArtist);
+
+        JSlider playbackSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+        playbackSlider.setBounds( getWidth()/2 - 300/2, 365, 300, 40);
+        playbackSlider.setBackground(null);
+        add(playbackSlider);
+
+        addPlaybackBtns();
 
     }
 
-    private ImageIcon loadImage(String imagePath){
-        try{
-            BufferedImage image =  ImageIO.read(new File(imagePath));
+    private ImageIcon loadImage(String imagePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(imagePath));
             return new ImageIcon(image);
-        }catch(Exeption e){
+        } catch (Exception e) {
             e.printStackTrace();
-
         }
+        return null;
     }
 
+    private void addPlaybackBtns(){
+        JPanel playbackBtns = new JPanel();
+        playbackBtns.setBounds(0,435,getWidth() - 10,80);
+        playbackBtns.setBackground(null);
+
+        JButton prevButton = new JButton(loadImage("src\\assets\\previous.png"));
+        prevButton.setBorderPainted(false);
+        prevButton.setBackground(null);
+        playbackBtns.add(prevButton);
+
+        JButton playButton = new JButton(loadImage("src\\assets\\play.png"));
+        playButton.setBorderPainted(false);
+        playButton.setBackground(null);
+        playbackBtns.add(playButton);
+
+        JButton pauseButton = new JButton(loadImage("src\\assets\\pause.png"));
+        pauseButton.setBorderPainted(false);
+        pauseButton.setVisible(false);
+        pauseButton.setBackground(null);
+        playbackBtns.add(pauseButton);
+
+        JButton nextButton = new JButton(loadImage("src\\assets\\next.png"));
+        nextButton.setBorderPainted(false);
+        nextButton.setBackground(null);
+        playbackBtns.add(nextButton);
+
+        add(playbackBtns);
+    }
 
     private void addToolbar() {
         JToolBar toolBar = new JToolBar();
